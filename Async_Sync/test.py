7c2388ccf7bf8336e43a74e36340f611739e609e
SimaoQuintela <simaopedroquintela@hotmail.com>
2022-07-13 13:04:04 +0100

    Async/Sync resources update

--- Async_Sync/test.py
+++ Async_Sync/test.py
@@ -12,13 +12,29 @@ async def print_vowels():
 
     for vowel in vowels:
         print(vowel)
+        await asyncio.sleep(0.25)
 
+
+async def print_my_name(): # sou bué egocêntrico
+    my_name = ["Simao", "Pedro", "Batista", "Caridade", "Quintela"]
+
+    for name in my_name:
+        print(name)
+        await asyncio.sleep(1)
+        
+
+
+# Question: What's happening here?
+# Answer: With the create_task method, I'm adding the tasks to the event pool. In the event pool the
+# tasks are being executed asynchronously and with the await function I'm saying "wait for this function to end" before moving forward
 async def main():
     task1 = asyncio.create_task(print_numbers())
     task2 = asyncio.create_task(print_vowels())
-
+    task3 = asyncio.create_task(print_my_name())
+    
     await task1
     await task2
+    await task3 
 
     print("Finished")
 

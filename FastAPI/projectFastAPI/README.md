c47becc8baf2a05c0b13bfb5c78f907a539134e5
fbarros86 <xiquita.pitoca@gmail.com>
2022-07-13 22:11:53 +0100

    FastAPI project - structure and creates

---
+++ FastAPI/projectFastAPI/README.md
@@ -0,0 +1,3 @@
+## Project to consolidate what we have learnt about FastAPI, SQLAlchemy, Pydantic and Alembic
+
+An implementation of an API associated with a database with information about prices of diferente products in different stores
---
+++ FastAPI/projectFastAPI/src/models.py
@@ -0,0 +1,30 @@
+from itertools import product
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class Store(Base):
+    __tablename__ = "stores"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    address = Column(String, default="")
+    productStore = relationship("Product", back_populates="priceStore")
+
+class Product(Base):
+    __tablename__ = "store_products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    price = Column(Integer,index=True)
+    store = Column(Integer, ForeignKey("stores.id"))
+    product = Column(Integer, ForeignKey("products.id"))
+    priceStore = relationship("Store", back_populates="productStore")
+    priceProduct = relationship("ProductType", back_populates="productType")
+    
+
+class ProductType(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    productType = relationship("Product", back_populates="priceProduct")

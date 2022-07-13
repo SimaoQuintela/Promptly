a46676b9166f05b21797f08c6d27719e3631879a
Simao Quintela <simaopedroquintela@hotmail.com>
2022-07-13 22:38:47 +0100

    project done

--- Project/database.py
+++ Project/database.py
@@ -1,4 +1,8 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
 
-engine = create_engine("postgresql://postgres:nathanj35@localhost/")
+engine = create_engine("postgresql://postgres:nathanj35@localhost/item_db", echo=True, future=True)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine)
--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from xxlimited import new
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+from database import SessionLocal
+import models
 
 app = FastAPI()
 
@@ -12,29 +15,67 @@ class Item(BaseModel): ## serializer
     price: int
     on_offer: bool
 
-
-@app.get('/')
-def index():
-    return {"message": "Hello World!"}
-
-@app.get('/greet/{name}')
-def greet_name(name: str):
-    return {"greeting": f"Hello {name}!"}
+    class Config:
+        orm_mode=True
 
 
-@app.get('/greet') 
-def greet_optional_name(name: Optional[str]="user"):
-#                                          |
-#           In case the user don't provide a name it'll be by default "user"
-    return {"message": f"Hello {name}!"}
+db = SessionLocal()
+
+@app.get('/items', response_model=List[Item], status_code=200)
+def get_all_items():
+    items = db.query(models.Item).all()
+
+    return items
+
+@app.get('/item/{item_id}', response_model=Item, status_code=status.HTTP_200_OK)
+def get_item(item_id: int):
+    item = db.query(models.Item).filter(models.Item.id == item_id).first()
+
+    return item
 
 
+@app.post('/items', response_model=Item, status_code=status.HTTP_201_CREATED)
+def create_an_item(item: Item):
+    db_item = models.query(models.Item).filter(models.Item.name == item.name).first()
 
-@app.put('/item/{item_id}')
-def update_item(item_id: int, item: Item):
-    return {
-            'name': item.name,
-            'description': item.description,
-            'price': item.price,
-            'on_offer': item.on_offer
-        }
+    if db_item is not None:
+        raise HTTPException(status_code=400, detail="Item already exists")
+
+    new_item = models.Item(
+        name = item.name,
+        price = item.price,
+        description = item.description,
+        on_offer = item.on_offer
+    )
+    
+
+    db.add(new_item)
+    db.commit()
+
+    return new_item
+
+
+@app.put('/item/{item_id}', response_model=Item, status_code=status.HTTP_200_OK)
+def update_an_item(item_id: int, item: Item):
+    item_to_update = db.query(models.Item).filter(models.Item.id == item_id).first()
+
+    item_to_update.name = item.name
+    item_to_update.price = item.price
+    item_to_update.description = item.description
+    item_to_update.on_offer = item.on_offer
+
+    db.commit()
+
+    return item_to_update
+
+@app.delete('/item/{item_id}')
+def delete_item(item_id: int):
+    item_to_delete = db.query(models.Item).filter(models.Item.id == item_id)
+
+    if item_to_delete is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="resource not found")
+    
+    db.delete(item_to_delete)
+    db.commit()
+
+    return item_to_delete
